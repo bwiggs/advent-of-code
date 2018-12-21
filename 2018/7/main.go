2dd46dd98d958d4be6129b7171b98603f06fb027
Brian Wigginton <brianwigginton@gmail.com>
2018-12-21 16:52:27 -0600

    day 7 ongoing

--- 2018/7/main.go
+++ 2018/7/main.go
@@ -6,47 +6,66 @@ import (
 	"strings"
 
 	"github.com/bwiggs/advent-of-code/2018/lib"
+	"github.com/davecgh/go-spew/spew"
 )
 
+func main() {
+	graph := buildGraph()
+	graph.Print()
+}
+
+func buildGraph() *Node {
+	var root *Node
+	list := []string{}
+	for _, l := range lib.ReadLines("input.txt") {
+		fmt.Printf("\n----- %s -----\n", l)
+		words := strings.Split(l, " ")
+
+		parentName := words[1]
+		childName := words[7]
+
+		list = append(list, parentName+childName)
+
+		if root == nil {
+			fmt.Printf("root: %s\n", parentName)
+			root = NewNode(parentName)
+		}
+
+		var depNode *Node
+		if depNode = root.Find(childName); depNode == nil {
+			fmt.Printf("creating: %s\n", childName)
+			depNode = NewNode(childName)
+		}
+
+		fmt.Printf("inserting: %s -> %s\n", depNode.Name, parentName)
+		if root == nil {
+			root = NewNode(parentName)
+		}
+		root.Insert(parentName, depNode)
+	}
+
+	sort.Slice(list, func(i, j int) bool { return list[i] < list[j] })
+	fmt.Println(list)
+
+	spew.Dump(root)
+
+	return root
+}
+
+// Node type
 type Node struct {
 	Name     string
 	Children []*Node
 	Prereqs  int
+	Visited  bool
 }
 
-func main() {
-	graph := getNodes()
-	graph.Print()
+// NewNode returns a new Node type
+func NewNode(name string) *Node {
+	return &Node{name, []*Node{}, 0, false}
 }
 
-func getNodes() *Node {
-	var root *Node
-	for _, l := range lib.ReadLines("test-input.txt") {
-		words := strings.Split(l, " ")
-
-		nodeName := words[1]
-		childName := words[7]
-
-		if root == nil {
-			// fmt.Println("created root node")
-			root = &Node{nodeName, []*Node{&Node{childName, []*Node{}}}}
-			continue
-		}
-
-		// fmt.Println("new node read!")
-
-		var depNode *Node
-		if depNode = root.Find(childName); depNode == nil {
-			// fmt.Printf("creating new node: %s\n", childName)
-			depNode = NewNode(childName)
-		}
-
-		// fmt.Printf("inserting: %s -> %s\n", nodeName, depNode.Name)
-		root.Insert(nodeName, depNode)
-	}
-	return root
-}
-
+// Find node
 func (n *Node) Find(nodeName string) *Node {
 	if n.Name == nodeName {
 		return n
@@ -61,18 +80,40 @@ func (n *Node) Find(nodeName string) *Node {
 	return nil
 }
 
+// Print node tree
 func (n *Node) Print() {
 	fmt.Print(n.Name)
+
+	// remove 1 dependnency from all children
 	for _, c := range n.Children {
-		c.Prereqs++
+		c.Prereqs--
+		if !c.Visited && c.Prereqs == 0 {
+			c.Print()
+			// nc++
+		}
 	}
+
+	// execute any children that are ready
+	// nc := 0
+	// for i := 0; nc != len(n.Children); i++ {
+	// 	if !n.Children[i].Visited && n.Children[i].Prereqs == 0 {
+	// 		n.Children[i].Print()
+	// 		nc++
+	// 	}
+	// 	if i == len(n.Children)-1 {
+	// 		i = -1
+	// 	}
+	// }
+	// n.Visited = true
 }
 
+// Insert node
 func (n *Node) Insert(nodeName string, child *Node) (inserted bool) {
 	// fmt.Printf("visiting: %s\n", n.Name)
 	if n.Name == nodeName {
 		n.Children = append(n.Children, child)
 		sort.Slice(n.Children, func(i, j int) bool { return n.Children[i].Name < n.Children[j].Name })
+		child.Prereqs++
 		return true
 	}
 
@@ -85,8 +126,12 @@ func (n *Node) Insert(nodeName string, child *Node) (inserted bool) {
 	return false
 }
 
-func NewNode(name string) *Node {
-	return &Node{name, []*Node{}}
+func (n *Node) String() string {
+	s := fmt.Sprintf("%s: ", n.Name)
+	for _, c := range n.Children {
+		s += c.Name
+	}
+	return s
 }
 
 // --- Day 7: The Sum of Its Parts ---
